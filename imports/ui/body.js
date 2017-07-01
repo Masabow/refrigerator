2bee3510f2f096c39e4d1f0920aa840a0e6af779
masaya <m.fuyuhiro@gmail.com>
2017-07-02 08:20:12 +0900

    Add ignore files

---
+++ imports/ui/body.js
@@ -0,0 +1,29 @@
+import { Template } from 'meteor/templating';
+
+import { Items } from '../api/items.js';
+ 
+import './body.html';
+
+Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  this.counter = new ReactiveVar(0);
+});
+
+Template.hello.helpers({
+  counter() {
+    return Template.instance().counter.get();
+  },
+});
+
+Template.hello.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+  },
+});
+
+Template.body.helpers({
+  items(){
+    return Items.find({});
+  },
+});
